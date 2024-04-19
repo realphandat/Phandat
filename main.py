4307bcdc43025734702ecebfe91b2ce89b8964f2
realphandat <155445899+realphandat@users.noreply.github.com>
2024-04-19 18:38:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ class MyClient(discord.Client, data):
 		self.solve_captcha_again = True
 		self.gem_check = True
 		self.gem_recheck = True
-		self.work_time = random.randint(600, 1200)
+		self.work_time = random.randint(300, 600)
 		self.cmd_amount = 0
 		self.captcha_amount = 0
 		self.gem_amount = 0
@@ -250,7 +250,7 @@ class MyClient(discord.Client, data):
 			await asyncio.sleep(random.randint(3, 5))
 			check = None
 			async for message in self.OwO.dm_channel.history(limit=1):
-						if message.author.id == self.OwOID and (await self.get_messages(message, "verified") or await self.get_messages(message, "Worry")):
+						if message.author.id == self.OwOID and (await self.get_messages(message, "verified") or await self.get_messages(message, "Wrong")):
 							check = message
 			if "verified" in check.content and check.author.id == self.OwOID:
 				print(f"{await self.intro()}{color.blue}[INFO]{color.reset} {color.bold}I Solved Image Captcha{color.reset} {color.green}Successfully!{color.reset}")
@@ -660,13 +660,13 @@ class MyClient(discord.Client, data):
 			await self.channel.send(say)
 			print(f"{await self.intro()}{color.yellow}[SEND] {say}{color.reset}")
 			self.cmd_amount += 1
-			await asyncio.sleep(random.randint(1, 2))
+			await asyncio.sleep(random.randint(6, 8))
 		if self.owo_status and self.work and self.feature['grind']:
 			await self.channel.typing()
 			await self.channel.send(f"{self.info['prefix']}h")
 			print(f"{await self.intro()}{color.yellow}[SEND] {self.info['prefix']}h{color.reset}")
 			self.cmd_amount += 1
-			await asyncio.sleep(random.randint(1, 2))
+			await asyncio.sleep(random.randint(6, 8))
 		if self.owo_status and self.work and self.feature['grind']:
 			await self.channel.typing()
 			await self.channel.send(f"{self.info['prefix']}b")
@@ -900,7 +900,7 @@ class MyClient(discord.Client, data):
 									description = f"<a:Arrow:1065047400714088479>I'm Taking A Break For **__{sleep_time}__ Seconds**",
 									color = 0xA2B5CD)
 			await asyncio.sleep(sleep_time)
-			self.work_time = random.randint(600, 1200)
+			self.work_time = random.randint(300, 600)
 			print(f"{await self.intro()}{color.blue}[INFO]{color.reset} {color.bold}Done! I'll Work For{color.reset} {color.cyan}{self.work_time} Seconds{color.reset}")
 			await self.send_webhooks(title = "**🌄 WAKE UP 🌄**",
 									description = f"<a:Arrow:1065047400714088479>I'll Work For **__{self.work_time}__ Seconds**",
