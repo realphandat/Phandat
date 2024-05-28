527dbdfd080caab1993a98b9704dfd916cd98ecb
realphandat <155445899+realphandat@users.noreply.github.com>
2024-05-28 18:44:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -563,7 +563,7 @@ class MyClient(discord.Client, data):
 		#Commands
 		if self.webhook['mode'] and message.author.id in self.webhook['owner_id']:
 			#Help
-			if message.content.lower() == "start":
+			if message.content.lower() == "help":
 				print(f"{await self.intro()}{color.blue}[INFO]{color.reset} {color.bold}Send Stat{color.reset} {color.gray}Via Webhook{color.reset}")
 				await self.send_webhooks(
 					title = f"📋 HELP MENU 📋",
@@ -571,7 +571,7 @@ class MyClient(discord.Client, data):
 					color = 0x8B4513
 				)
 			#Start
-			if message.content.lower() == "help":
+			if message.content.lower() == "start":
 				if not self.selfbot['work_status']:
 					print(f"{await self.intro()}{color.blue}[INFO]{color.reset} {color.bold}Start{color.reset} {color.gray}Selfbot{color.reset}")
 					await self.send_webhooks(
